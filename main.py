1d68f51237965464114b144bd7f5806e92b9049d
Burak Yapici <byapici95@gmail.com>
2024-03-29 23:06:45 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import turtle
+
+drawing_board = turtle.Screen()
+drawing_board.bgcolor("orange")
+drawing_board.title("Selo Turtle")
+
+
+#square
+
+turtle_instance = turtle.Turtle()
+
+turtle_instance.forward(100)
+turtle_instance.right(150)
+turtle_instance.forward(100)
+turtle_instance.left(90)
+turtle_instance.forward(100)
+turtle_instance.right(150)
+turtle_instance.forward(100)
+turtle_instance.left(90)
+turtle_instance.forward(100)
+turtle_instance.right(150)
+turtle_instance.forward(100)
+turtle_instance.left(90)
+turtle_instance.forward(100)
+turtle_instance.right(150)
+turtle_instance.forward(100)
+turtle_instance.left(60)
+turtle_instance.forward(100)
+turtle_instance.right(130)
+turtle_instance.forward(120)
+
+
+
+
+
+
+
+
+
+
